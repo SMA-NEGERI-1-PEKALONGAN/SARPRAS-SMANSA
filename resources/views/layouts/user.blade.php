b1c38bf76131e4045033fbdb1c47c0fc6bcfd003
lammmzt <alam@sma1pekalongan.sch.id>
2026-09-04 07:31:09 +0700

    update fiture

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -34,7 +34,7 @@
     
     {{-- pusher notification --}}
     <link rel="manifest" href="{{ asset('manifest.json') }}">
-    <meta name="theme-color" content="#2563eb">
+    {{-- <meta name="theme-color" content="#2563eb"> --}}
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
